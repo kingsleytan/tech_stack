5db9e69017ec7ef493da5c7f1441aefbddf21a32
Kingsley <sengking.tan@gmail.com>
2017-09-10 21:20:04 +0800

    added SelectionReducer

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,7 @@ import { CardSection } from './common';
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+    
     return (
     <CardSection>
       <Text style={titleStyle}>
